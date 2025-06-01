13a7a88da883f00618de6c3995e594d376cf7500
Kori Fogle <kfogl001@odu.edu>
2025-06-01 19:17:31 -0400

    Added folder to store regex expressions by emotion. Also added nbstripout for commits, so notebook metadata isn't tracked. For some reason git is saying i modified other files but there arent any changes there.

---
+++ patterns/happy.py
@@ -0,0 +1,61 @@
+happy_patterns = [
+(r".*\b(happy|happiness|joy|joyful|cheerful|delighted)\b.*",
+     "A good happy vibe! What’s got you feeling good, {name}?"),
+
+    (r".*\b(excited|thrilled|elated|ecstatic|overjoyed)\b.*",
+     "Someone’s riding the good wave! What’s the scoop, {name}?"),
+
+    (r".*\b(grateful|thankful|blessed)\b.*",
+     "Gratitude! What’s making your heart jump, {name}?"),
+
+    (r".*\b(smile|smiling|beaming|grinning)\b.*",
+     "What’s got you glowing, {name}?"),
+
+    (r".*\b(celebrate|celebration|party|partying)\b.*",
+     "What are we celebrating, {name}?"),
+
+    (r".*\b(awesome|amazing|great|fantastic|fabulous|wonderful)\b.*",
+     "That’s a whole vibe right there! What made your day so awesome, {name}?"),
+
+    (r".*\b(chill|relaxed|laid back|zen|breezy)\b.*",
+     "Chill mode. What’s helping you float like this, {name}?"),
+
+    (r".*\b(loved|love|loving)\b.*",
+     "Who or what’s getting all this affection, {name}?"),
+
+    (r".*\b(good mood|feeling good|good vibes|great day)\b.*",
+     "Catchin’ them good mood rays ☀️ What sparked it, {name}?"),
+
+    (r".*\b(lucky|fortunate|on a roll|everything’s clicking)\b.*",
+     "Jackpot energy 🎰 What’s going your way today, {name}?"),
+
+    (r".*\b(won|winning|accomplished|achieved)\b.*",
+     "What did you just crush, {name}?"),
+
+    (r".*\b(promotion|new job|offer|accepted|greenlighted)\b.*",
+     "Level up! Tell me all about the upgrade, {name}!"),
+
+    (r".*\b(good news|great update|awesome message)\b.*",
+     "Hit me with the headline, {name}!"),
+
+    (r".*\b(inspired|creative|fired up|pumped|motivated)\b.*",
+     "What are you cooking up, {name}?"),
+
+    (r".*\b(laughing|lol|lmao|giggle|cracking up|funny)\b.*",
+     "What cracked you up, {name}?"),
+
+    (r".*\b(vacation|beach|trip|travel|getaway)\b.*",
+     "Where are you off to, {name}?"),
+
+    (r".*\b(food|dessert|ice cream|pizza|snack|brunch)\b.*",
+     "What did you eat next, {name}?"),
+
+    (r".*\b(music|dancing|groove|beats|jammed out|playlist)\b.*",
+     "What was on the aux, {name}?"),
+
+    (r".*\b(friend|friends|hangout|catch up|reunion)\b.*",
+     "That sounds like a great hangout. Who’d you link up with, {name}?"),
+
+    (r".*\b(cleaned|organized|productive|got stuff done)\b.*",
+     "What’d you cross off the list, {name}?")
+]
